ca285f211716cf11af475058d558e3c3fb97076a
didenkodenys <didenko.denys17@gmail.com>
2024-10-02 17:53:55 +0300

    Add files via upload

--- preload.js
+++ preload.js
@@ -1,1 +1,10 @@
-//empty
+function setFullHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Устанавливаем значение при загрузке страницы
+setFullHeight();
+
+// Обновляем значение при изменении размера окна
+window.addEventListener('resize', setFullHeight);
